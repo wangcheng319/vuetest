9d5268c6c518c3a6d43a0c65cdceb7780b6b4ffe
wangc <274281610@qq.com>
2019-06-05 21:52:36 +0800

    动画

--- src/components/me/SubItem.vue
+++ src/components/me/SubItem.vue
@@ -1,5 +1,14 @@
 <template>
-    <div @click="itemClick">父组件传递过来的：{{this.nameP}}</div>
+    <div>
+        <div @click="itemClick">父组件传递过来的：{{this.nameP}}</div>
+
+        <button @click="toggle">动画-向左移动</button>
+        <!--动画都包裹在transition里面，name相当于重新命名-->
+        <transition name="right" >
+            <div class="to-right" v-show="isshow"></div>
+        </transition>
+    </div>
+
 </template>
 
 <!--父子组件之间的通信-->
@@ -8,15 +17,40 @@
         name: "SubItem",
         /*props 获取父组件传递过来的参数*/
         props:['nameP'],
+        data(){
+            return{
+                isshow : false
+            }
+        },
         methods:{
             itemClick(){
                 //子组件发送事件，前面为事件名，后面为参数
                 this.$emit('itemClick','我是子组件传递过来的参数')
+            },
+            toggle() {
+                this.isshow = !this.isshow;
             }
         }
     }
 </script>
 
 <style scoped>
+    .to-right{
+        width: 100px;
+        height: 100px;
+        background-color: aqua;
+    }
+
+    .right-enter,right-leave-to{
+        margin-left: 100px;
+        transition: 3s;
+    }
+
+    .right-enter-to,right-leave{
+        margin-left: 30px;
+        transition: 3s;
+    }
+
+
 
 </style>
